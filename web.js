1214e7b23d4456d0cfe358098439fea2778f3536
Damodar Karma <d_karmacharya@hotmail.com>
2013-08-15 05:19:27 +0000

    HW Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+fs.readFile('index.html',function(err,data){
+ if(err) throw err;
+    response.send(data);
+});
 });
 
 var port = process.env.PORT || 5000;
