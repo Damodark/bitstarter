abe6bcd0846e20becf6b62a0a31099ebb375c866
Damodar Karma <d_karmacharya@hotmail.com>
2013-08-15 05:57:40 +0000

    HW 2 Updated web.js

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
+var buffer = new Buffer(256);
 app.use(express.logger());
 
 app.get('/', function(request, response) {
 
-fs.readFile('index.html',function(err,data){
- if(err) throw err;
-    response.send(data);
+var file = fs.readFileSync('index.html');
+var filetext = file.tostring();
+response.send(filetext);
 });
-});
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
